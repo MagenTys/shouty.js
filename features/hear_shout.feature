d321ccf328a19688a6efa4f7ceadcd04c7ff6edf
Tim Myerscough <tim.myerscough@magentys.io>
2017-01-20 10:17:45 +0800

    implement sample feature

--- features/hear_shout.feature
+++ features/hear_shout.feature
@@ -1,3 +1,10 @@
 Feature: Hear Shout
 
-  A description goes here...
+  Rules:
+    - hear shout within 1000m
+
+  Scenario: Bill hears a shout
+    Given Bill is at location (0,0)
+    And Ben is at location (0,700)
+    When Ben shouts "Let's Kickstart"
+    Then Bill should hear the shout
--- features/step_definitions/shout_steps.js
+++ features/step_definitions/shout_steps.js
@@ -3,5 +3,21 @@ var Shouty = require('../../lib/shouty');
 
 module.exports = function () {
   this.World = function() {
+      this.shouty = new Shouty()
   };
+
+  this.Given(/^(\w+) is at location \((\d+),(\d+)\)$/, function (name, locX, locY, callback) {
+    this.shouty.addPerson(name,locX,locY);
+    callback()
+  });
+
+  this.When(/^(\w+) shouts "([^"]*)"$/, function (name, message, callback) {
+    this.shouty.person_shouts(name, message)
+    callback()
+  });
+
+  this.Then(/^(\w+) should hear the shout$/, function (name,callback) {
+    assert.ok(this.shouty.person(name).heard_shout())
+    callback()
+  });
 };
